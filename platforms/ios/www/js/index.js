a662865aac22cae0d18029cb1562585918bf7618
Justin Wallace <justin.wallace@momentumww.com>
2016-02-17 14:09:11 -0600

    Fixed console errors. This iteration stores all form data submitted and logs it to the console. Data can be cleared by clicking the "Clear Storage" button.

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -26,6 +26,9 @@ function hex2a(hex) {
     return str;
 }
 
+var formData,
+    data = [];
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -43,8 +46,9 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+
         app.receivedEvent('deviceready');
-    
+
         /**
          * Use these scanner types
          * Available: "PDF417", "USDL", "Bar Decoder", "Zxing", "MRTD", "UKDL", "MyKad"
@@ -52,7 +56,7 @@ var app = {
         // var types = ["Bar Decoder", "USDL", "PDF417", "UKDL", "MRTD", "Zxing"];
         var types = ["USDL", "PDF417"];
 
-        /* license associated with com.momentumww.bluScanner */
+        /* license associated with com.momentumww.bluScanner DEMO ONLY */
         var licenseiOs = "IZXZT3SI-BSSEETU3-OI7PX2JU-XCLIARHD-G6BL6LIY-CH7IMO6F-L7UTJOEW-RDXSFGTV";
 
         // Remove warning if necessary
@@ -71,6 +75,8 @@ var app = {
         scanButton.addEventListener('click', function() { 
             // Remove previous warning labels, if any
             $('.warn').removeClass('warn');
+            // Reset form fields
+            $('#bluForm').reset();
             
             cordova.plugins.blinkIdScanner.scan(
             
@@ -193,38 +199,7 @@ var app = {
                             } else {
                                 console.log(ZIP + ' is undefined')
                             }
-
-
                         }                  
-
-                        // } else if (recognizerResult.resultType == "Zxing result") {
-                            
-                        //     var fields = recognizerResult.fields;
-
-                        //     resultDiv.innerHTML = /** Personal information */
-                        //                         "ID Type: " + fields[kPPmyKadDataType] + "; " +
-                        //                         "NRIC Number: " + fields[kPPmyKadNricNumber] + "; " +
-                        //                         "Address: " + fields[kPPmyKadAddress] + "; " +
-                        //                         "Birth Date: " + fields[kPPmyKadBirthDate] + "; " +
-                        //                         "Full Name: " + fields[kPPmyKadFullName] + "; " +
-                        //                         "Religion: " + fields[kPPmyKadReligion] + "; " +
-                        //                         "Sex: " + fields[kPPmyKadSex] + "; "; +
-                        //                         "Data: " + recognizerResult.data + raw + " (Type: " + recognizerResult.type + ")";
-                        // }
-
-                        // } else if (recognizerResult.resultType == "MyKad result") {
-                            
-                        //     var fields = recognizerResult.fields;
-
-                        //     resultDiv.innerHTML = /** Personal information */
-                        //                         "ID Type: " + fields[kPPmyKadDataType] + "; " +
-                        //                         "NRIC Number: " + fields[kPPmyKadNricNumber] + "; " +
-                        //                         "Address: " + fields[kPPmyKadAddress] + "; " +
-                        //                         "Birth Date: " + fields[kPPmyKadBirthDate] + "; " +
-                        //                         "Full Name: " + fields[kPPmyKadFullName] + "; " +
-                        //                         "Religion: " + fields[kPPmyKadReligion] + "; " +
-                        //                         "Sex: " + fields[kPPmyKadSex] + "; ";
-                        // }
                     }
                 },
                 
@@ -242,173 +217,180 @@ var app = {
     receivedEvent: function(id) {
         console.log('Received Event: ' + id);
     }
+};
 
-    // Mandrill Code
-    jQuery(function($)  
-    {
-        $("#enter").click(function ()
-        {
-            email = $("#email").val(); // get email field value
-
-            if (isValidEmailAddress(email)) {
-
-              $.ajax(
-              {
-                type: "POST",
-                url: "https://mandrillapp.com/api/1.0/messages/send-template.json",
-                data: {
-                   "key": "vV0UyEfydJwLYC8XoC1NwA",
-                   "template_name": "ritz-welcome-email-1", //ritz-welcome-email-1 for team 2 | ritz-test for testing
-                   "template_content": [
-                     {
-                       "name": "example name",
-                       "content": "example content"
-                     }
-                   ],
-                   "message": {
-                      "from_email": "snacks@ritzcrackers.com",
-                      "to": [
-                          {
-                           "email": email, //$("#reg-form").serialize(),
-                           "type": "to"
-                          }
-                        ],
-                      "subject": "Get snacking with these Big Ritz Snack Truck Recipes."
-                    }
-                  }
-              })
-              .done(function() {
-                mandrillFired = true;
-                //console.log(mandrillFired);
-                emails = window.localStorage.getArray('emails');
-                arrayLength = emails.length;
-                if (arrayLength > 0 ) {
-                  sendBacklog();
-                }
-                signupComplete();
-              })
-            } else {
-              // Set the email to red
-              $('#email').css('color', 'red');
-              // Then make it black again after a timeout
-              setTimeout(function(){$('#email').css('color', 'black');}, 500);
-              alert("Please enter a valid email address!");
-            }
-
-            setTimeout(function(){
-              if (mandrillFired != true) {
-                console.log('Error sending message.');
-                // save the mail to localstorage, if offline
-                window.localStorage.pushArrayItem('emails', email);
-                signupComplete();
-              }
-            }, 1400);
-
-            return false; // prevent page refresh
-        });
+// Buttons & Actions
+jQuery(function($)  
+{
+    $('#clearStorage').click(function () {
+        window.localStorage.deleteArray('data');
+        console.log('Storage deleted');
     });
 
-    function sendBacklog(emails) {
-      // console.log("sending backlog...");
+    $('#submit').click(function () {
+        formData = $('form').serializeArray();
+        mandrillFired = true;
+        console.log(formData);
+        window.localStorage.pushArrayItem('data', formData);
 
-      emails = window.localStorage.getArray('emails');
+        data = window.localStorage.getArray('data');
 
-      for (var i = 0; i < (emails.length); i++) {
-        // console.log(email);
-        // console.log(emails[i]);
-        if (emails[i] != email) {
-          $.ajax(
-          {
+        for (var i = 0; i < (data.length); i++) {
+            console.log(data[i]);
+            if (data[i] != formData) {
+                // $.ajax({
+                //     type: "POST",
+                //     url: "https://mandrillapp.com/api/1.0/messages/send-template.json",
+                //     data: {
+                //         "key": "vV0UyEfydJwLYC8XoC1NwA",
+                //             "template_name": "ritz-welcome-email-1", //ritz-welcome-email-1 for team 2 | ritz-test for testing
+                //             "template_content": [
+                //             {
+                //                "name": "example name",
+                //                "content": "example content"
+                //             }
+                //         ],
+                //         "message": {
+                //             "from_email": "snacks@ritzcrackers.com",
+                //             "to": [
+                //             {
+                //                 "email": emails[i], //$("#reg-form").serialize(),
+                //                 "type": "to"
+                //              }
+                //             ],
+                //             "subject": "Get snacking with these Big Ritz Snack Truck Recipes."
+                //         }
+                //     }
+
+                //     }).done(function(response) {
+                //         console.log("Backlog sent");
+                //         backlogFired = true;
+                //     }).fail(function(response) {
+                //         console.log("Backlog wasn't sent");
+                //         backlogFired = false;
+                //     });
+            }
+        }
+
+        return false; // prevent page refresh
+    });
+});
+
+function signupComplete() {
+    console.log('signup complete');
+}
+
+function sendBacklog(emails) {
+    // console.log("sending backlog...");
+
+    emails = window.localStorage.getArray('emails');
+
+    for (var i = 0; i < (emails.length); i++) {
+    // console.log(email);
+    // console.log(emails[i]);
+    if (emails[i] != email) {
+        $.ajax({
             type: "POST",
             url: "https://mandrillapp.com/api/1.0/messages/send-template.json",
             data: {
-               "key": "vV0UyEfydJwLYC8XoC1NwA",
-                   "template_name": "ritz-welcome-email-1", //ritz-welcome-email-1 for team 2 | ritz-test for testing
-                   "template_content": [
-                     {
+                "key": "vV0UyEfydJwLYC8XoC1NwA",
+                    "template_name": "ritz-welcome-email-1", //ritz-welcome-email-1 for team 2 | ritz-test for testing
+                    "template_content": [
+                    {
                        "name": "example name",
                        "content": "example content"
+                    }
+                ],
+                "message": {
+                    "from_email": "snacks@ritzcrackers.com",
+                    "to": [
+                    {
+                        "email": emails[i], //$("#reg-form").serialize(),
+                        "type": "to"
                      }
-                   ],
-               "message": {
-                 "from_email": "snacks@ritzcrackers.com",
-                 "to": [
-                     {
-                       "email": emails[i], //$("#reg-form").serialize(),
-                       "type": "to"
-                     }
-                   ],
-                 "subject": "Get snacking with these Big Ritz Snack Truck Recipes."
-               }
+                    ],
+                    "subject": "Get snacking with these Big Ritz Snack Truck Recipes."
+                }
             }
-          })
-          .done(function(response) {
-            console.log("Backlog email sent");
-            backlogFired = true;
-          })
-          .fail(function(response) {
-            console.log("Backlog wasn't sent");
-            backlogFired = false;
-          });
+
+            }).done(function(response) {
+                console.log("Backlog email sent");
+                backlogFired = true;
+            }).fail(function(response) {
+                console.log("Backlog wasn't sent");
+                backlogFired = false;
+            });
         }
-      }
+    }
 
-      console.log("backlogFired = " + backlogFired);
+    console.log("backlogFired = " + backlogFired);
 
-      if (backlogFired != false) {
+    if (backlogFired != false) {
         console.log("emails array deleted");
         window.localStorage.deleteArray('emails');
-      }
     }
+}
 
-    function isValidEmailAddress(email) {
-      var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-      return pattern.test(email);
-    };
-
-    //Local Storage Extensions
-    Storage.prototype.getArray = function(arrayName) {
-      var thisArray = [];
-      var fetchArrayObject = this.getItem(arrayName);
-      if (typeof fetchArrayObject !== 'undefined') {
+//Local Storage Extensions
+Storage.prototype.getArray = function(arrayName) {
+    var thisArray = [];
+    var fetchArrayObject = this.getItem(arrayName);
+    if (typeof fetchArrayObject !== 'undefined') {
         if (fetchArrayObject !== null) { thisArray = JSON.parse(fetchArrayObject); }
-      }
-      return thisArray;
     }
+    return thisArray;
+}
 
-    Storage.prototype.pushArrayItem = function(arrayName,arrayItem) {
-      var existingArray = this.getArray(arrayName);
-      existingArray.push(arrayItem);
-      this.setItem(arrayName,JSON.stringify(existingArray));
-    }
+Storage.prototype.pushArrayItem = function(arrayName,arrayItem) {
+    var existingArray = this.getArray(arrayName);
+    existingArray.push(arrayItem);
+    this.setItem(arrayName,JSON.stringify(existingArray));
+}
 
-    Storage.prototype.popArrayItem = function(arrayName) {
-      var arrayItem = {};
-      var existingArray = this.getArray(arrayName);
-      if (existingArray.length > 0) {
+Storage.prototype.popArrayItem = function(arrayName) {
+    var arrayItem = {};
+    var existingArray = this.getArray(arrayName);
+    if (existingArray.length > 0) {
         arrayItem = existingArray.pop();
         this.setItem(arrayName,JSON.stringify(existingArray));
-      }
-      return arrayItem;
     }
+    return arrayItem;
+}
 
-    Storage.prototype.shiftArrayItem = function(arrayName) {
-      var arrayItem = {};
-      var existingArray = this.getArray(arrayName);
-      if (existingArray.length > 0) {
+Storage.prototype.shiftArrayItem = function(arrayName) {
+    var arrayItem = {};
+    var existingArray = this.getArray(arrayName);
+    if (existingArray.length > 0) {
         arrayItem = existingArray.shift();
         this.setItem(arrayName,JSON.stringify(existingArray));
-      }
-      return arrayItem;
     }
+    return arrayItem;
+}
 
-    Storage.prototype.unshiftArrayItem = function(arrayName,arrayItem) {
-      var existingArray = this.getArray(arrayName);
-      existingArray.unshift(arrayItem);
-      this.setItem(arrayName,JSON.stringify(existingArray));
-    }
+Storage.prototype.unshiftArrayItem = function(arrayName,arrayItem) {
+    var existingArray = this.getArray(arrayName);
+    existingArray.unshift(arrayItem);
+    this.setItem(arrayName,JSON.stringify(existingArray));
+}
 
-    Storage.prototype.deleteArray = function(arrayName) {
-      this.removeItem(arrayName);
+Storage.prototype.deleteArray = function(arrayName) {
+    this.removeItem(arrayName);
+}
+
+// Expanding local storage to deal with arrays
+Storage.prototype.setObj = function(key, obj) {
+    return this.setItem(key, JSON.stringify(obj))
+}
+Storage.prototype.getObj = function(key) {
+    return JSON.parse(this.getItem(key))
+}
+
+// Detect user keypress to hide iPad keyboard
+$(document).keypress(function(e) {
+    //if user presses go/return/enter
+    if(e.which == 13) {
+        //hide ipad keyboard
+        document.activeElement.blur();
+        $("input").blur();
     }
-};
+});
